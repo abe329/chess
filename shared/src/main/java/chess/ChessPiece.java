f722bf4bb65b8147ac1a0e9ea19e3bcda2f5c638
abe329 <abehull23@gmail.com>
2025-09-12 11:48:19 -0600

    imp(p0) Added first draft of moves for bishop and rook

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +54,56 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            // return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+            int[][] options = {{1, 1}, {-1, 1}, {1, -1}, {-1,-1}};
+            ArrayList<ChessMove> moves = new ArrayList<>();
+
+            for(int[] i : options) {
+                int dx = i[0];
+                int dy = i[1];
+                int row = myPosition.getRow() + dx;
+                int col = myPosition.getColumn() + dy;
+
+                while(isOnBoard(row, col)) {
+                    // System.out.println(String.format("%d, %d -- dx: %d, dy: %d", row, col, dx, dy));
+                    ChessPosition newPosition = new ChessPosition(row, col);
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                    row += dx;
+                    col += dy;
+                    }
+            }
+
+            return moves;
+        }
+        if (piece.getPieceType() == PieceType.ROOK) {
+            // return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+            int[][] options = {{0, 1}, {0, -1}, {1, 0}, {-1,0}};
+            ArrayList<ChessMove> moves = new ArrayList<>();
+
+            for(int[] i : options) {
+                int dx = i[0];
+                int dy = i[1];
+                int row = myPosition.getRow() + dx;
+                int col = myPosition.getColumn() + dy;
+
+                while(isOnBoard(row, col)) {
+                    ChessPosition newPosition = new ChessPosition(row, col);
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                    row += dx;
+                    col += dy;
+                }
+            }
+
+            return moves;
+        }
+
         return List.of();
     }
+    private boolean isOnBoard(int row, int col) {
+        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+    }
+
+
 }
