a6cbde4ef76c5966f19dc3988859ec47f104b6e8
abe329 <abehull23@gmail.com>
2025-09-19 12:48:02 -0600

    imp(p0) added pawn movements!

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -14,10 +14,15 @@ import java.util.Collection;
 public class PieceMovesCalculator {
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
-        PieceMovement pm = PieceMovement.valueOf(piece.getPieceType().name());
-        int[][] directions = pm.directions;
-        boolean slider = pm.slider;
+        PieceMovement pieceMoves = PieceMovement.valueOf(piece.getPieceType().name());
+        int[][] directions = pieceMoves.directions;
+        boolean slider = pieceMoves.slider;
         ArrayList<ChessMove> moves = new ArrayList<>();
+
+        // PawnMovement does all pawn moves
+        if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
+            return PawnMovement(board, myPosition);
+        }
 
         for(int[] i : directions) {
             int dx = i[0];
@@ -27,8 +32,14 @@ public class PieceMovesCalculator {
 
             while(isOnBoard(row, col)) {
                 ChessPosition newPosition = new ChessPosition(row, col);
-                if (emptySquare(board, row, col)) {
+                ChessPiece target = board.getPiece(newPosition);
+                if (target == null) {
                     moves.add(new ChessMove(myPosition, newPosition, null));
+                } else {
+                     if (target.getTeamColor() != piece.getTeamColor()) {
+                         moves.add(new ChessMove(myPosition, newPosition, null));
+                     }
+                     break;
                 }
                 if (!slider) break; //Knight and King don't move continuously
 
@@ -56,12 +67,40 @@ public class PieceMovesCalculator {
         }
     }
 
-    private boolean isOnBoard(int row, int col) {
-        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+    private Collection<ChessMove> PawnMovement(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        int pawnDirection;
+        // real proud of this one :)
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) pawnDirection= 1; else { pawnDirection= -1; }
+        int pawnRow = row + pawnDirection;
+
+        if (isOnBoard(pawnRow, col) && board.getPiece(new ChessPosition(pawnRow, col)) == null) {
+            // Base move
+            moves.add(new ChessMove(myPosition, new ChessPosition(pawnRow, col), null));
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE && row == 2 || (piece.getTeamColor() == ChessGame.TeamColor.BLACK && row == 7)) {
+                // Initial 2 square move
+                if (board.getPiece(new ChessPosition(pawnRow + pawnDirection, col)) == null) {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(pawnRow + pawnDirection, col), null));
+                }
+            }
+        }
+        int[] captureColumns = {col - 1, col + 1};
+        for (int cCol : captureColumns) {
+            if(isOnBoard(pawnRow, cCol)) {
+                ChessPiece target = board.getPiece(new ChessPosition(pawnRow, cCol));
+                if(target != null && target.getTeamColor() != piece.getTeamColor()) {
+                    moves.add(new ChessMove(myPosition,new ChessPosition(pawnRow, cCol), null));
+                }
+            }
+        }
+
+        return moves;
     }
 
-    private boolean emptySquare(ChessBoard board, int row, int col) {
-        ChessPosition position = new ChessPosition(row, col);
-        return board.getPiece(position) == null;
+    private boolean isOnBoard(int row, int col) {
+        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
     }
 }
