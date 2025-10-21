c74eaddb27971c0de20e71f7edba248981dee7af
abe329 <abehull23@gmail.com>
2025-10-20 23:17:41 -0600

    imp(p3) rough draft of clear/user interface + implementation

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,17 @@
+package dataaccess;
+
+import model.AuthData;
+import model.UserData;
+import model.GameData;
+// 나중에 optional 라는 도서관 써야할 수도있다
+
+public interface DataAccess {
+    void clear();
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+    UserData createAuthToken(String authToken) throws DataAccessException;
+
+    // Keep sticking more mini interfaces in to this MEGA interface
+}
---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,38 @@
+package dataaccess;
+
+import model.UserData;
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+    @Override
+    public void clear() {
+        users.clear();
+        auths.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("Error: already taken");
+        }
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.username(), auth);
+    }
+
+    public AuthData getAuthToken(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+}
