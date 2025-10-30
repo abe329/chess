90d14ace1bfc11c8d7172ed56b7b433cd948e5bb
abe329 <abehull23@gmail.com>
2025-10-29 23:58:52 -0600

    setup(p4) created mysql implementation file, and started filling in

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,171 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import model.*;
+
+import java.sql.*;
+import java.util.Map;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
+public class MySqlDataAccess implements DataAccess {
+
+    public MySqlDataAccess() throws DataAccessException {
+        configureDatabase();
+    }
+    public void clear() {
+
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        var statement = "INSERT INTO pet (username, password, email, json) VALUES (?, ?, ?)";
+        String json = new Gson().toJson(user);
+        int id = executeUpdate(statement, user.username(), user.password(), user.email(), json);
+        return new UserData(id, user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+
+    }
+
+    public void createAuth(AuthData auth) {
+
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    public void createGame(GameData game) throws DataAccessException {
+
+    }
+
+    public GameData getGame(Integer gameID) throws DataAccessException {
+
+    }
+
+    public Map<Integer, GameData> listGames(String authToken) throws DataAccessException {
+
+    }
+
+    public void joinGame(GameData game) throws DataAccessException {
+
+    }
+
+//    public Pet addPet(Pet pet) throws ResponseException {
+//        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
+//        String json = new Gson().toJson(pet);
+//        int id = executeUpdate(statement, pet.name(), pet.type(), json);
+//        return new Pet(id, pet.name(), pet.type());
+//    }
+//
+//    public Pet getPet(int id) throws ResponseException {
+//        try (Connection conn = DatabaseManager.getConnection()) {
+//            var statement = "SELECT id, json FROM pet WHERE id=?";
+//            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+//                ps.setInt(1, id);
+//                try (ResultSet rs = ps.executeQuery()) {
+//                    if (rs.next()) {
+//                        return readPet(rs);
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to read data: %s", e.getMessage()));
+//        }
+//        return null;
+//    }
+//
+//    public PetList listPets() throws ResponseException {
+//        var result = new PetList();
+//        try (Connection conn = DatabaseManager.getConnection()) {
+//            var statement = "SELECT id, json FROM pet";
+//            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+//                try (ResultSet rs = ps.executeQuery()) {
+//                    while (rs.next()) {
+//                        result.add(readPet(rs));
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to read data: %s", e.getMessage()));
+//        }
+//        return result;
+//    }
+//
+//    public void deletePet(Integer id) throws ResponseException {
+//        var statement = "DELETE FROM pet WHERE id=?";
+//        executeUpdate(statement, id);
+//    }
+//
+//    public void deleteAllPets() throws ResponseException {
+//        var statement = "TRUNCATE pet";
+//        executeUpdate(statement);
+//    }
+//
+//    private Pet readPet(ResultSet rs) throws SQLException {
+//        var id = rs.getInt("id");
+//        var json = rs.getString("json");
+//        Pet pet = new Gson().fromJson(json, Pet.class);
+//        return pet.setId(id);
+//    }
+//
+//    private int executeUpdate(String statement, Object... params) throws ResponseException {
+//        try (Connection conn = DatabaseManager.getConnection()) {
+//            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+//                for (int i = 0; i < params.length; i++) {
+//                    Object param = params[i];
+//                    if (param instanceof String p) ps.setString(i + 1, p);
+//                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+//                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+//                    else if (param == null) ps.setNull(i + 1, NULL);
+//                }
+//                ps.executeUpdate();
+//
+//                ResultSet rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    return rs.getInt(1);
+//                }
+//
+//                return 0;
+//            }
+//        } catch (SQLException e) {
+//            throw new ResponseException(ResponseException.Code.ServerError, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+//        }
+//    }
+////
+//    private final String[] createStatements = {
+//            """
+//            CREATE TABLE IF NOT EXISTS  pet (
+//              `id` int NOT NULL AUTO_INCREMENT,
+//              `name` varchar(256) NOT NULL,
+//              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+//              `json` TEXT DEFAULT NULL,
+//              PRIMARY KEY (`id`),
+//              INDEX(type),
+//              INDEX(name)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+//            """
+//    };
+//
+//
+//    private void configureDatabase() throws DataAccessException {
+//        DatabaseManager.createDatabase();
+//        try (Connection conn = DatabaseManager.getConnection()) {
+//            for (String statement : createStatements) {
+//                try (var preparedStatement = conn.prepareStatement(statement)) {
+//                    preparedStatement.executeUpdate();
+//                }
+//            }
+//        } catch (SQLException ex) {
+//            throw new DataAccessException("Error: Unable to configure database.");
+//            // throw new DataAccessException(DataAccessException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+//        }
+//    }
+}
